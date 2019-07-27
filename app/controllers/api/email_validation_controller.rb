18ade70b41466cb06c0abc0ca5b148c51b0ac272
Shilpa k <shilpasiva1510@gmail.com>
2019-07-27 19:47:17 +0530

    second

--- app/controllers/api/email_validation_controller.rb
+++ app/controllers/api/email_validation_controller.rb
@@ -1,19 +1,21 @@
 class Api::EmailValidationController < ApplicationController
   
-  def validate_email 
+  def validate_email
+    session[:email] = params[:from] 
     @article = Article.find(params[:article_id])
     @email = params[:from]
     ApplicationMailer.welcome_email(params[:from], @article.otp_code).deliver_now
   end
+
   def verify_otp
     @article = Article.find(params[:article_id])
     @status = {}
     if @article.authenticate_otp(params[:otp], drift: 300)
-EmailValidation.create(email: params[:email],  article_id: @article.id, status: true)
-  @status = { result: true, message: "Valid OTP", article_id: @article.id }
-else
-EmailValidation.create(email: params[:email],  article_id: @article.id, status: false)
-  @status = { result: false, message: "Invalid OTP"  }
-end
+      EmailValidation.create(email: params[:email],article_id: @article.id, status: true)
+      @status = { result: true, message: "Valid OTP", article_id: @article.id }
+    else
+      EmailValidation.create(email: params[:email],  article_id: @article.id, status: false)
+      @status = { result: false, message: "Invalid OTP"  }
+    end
   end
 end
